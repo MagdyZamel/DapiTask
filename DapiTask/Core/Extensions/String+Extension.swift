74c707164e9ee228e01721641d5a93440ca6842c
Magdy Zamel <Magdy.zamel@gmail.com>
2021-07-14 20:08:53 +0200

    feat: Add faviconDownloader and failure icon #2
    - Add failure icon
    - Add FaviconDownloaderProtocol
    Write FaviconDownloaderProtocol, to be the Protocol that used to any FaviconDownloader or FaviconDownloaderMock in testing
    To be implemented by FaviconDownloader and encapsulate the downloading implementation of the Favicon.
    Remove or replace FaviconFinder any time easily without any side effects
    
    - Add FaviconDownloader that implement FaviconDownloaderProtocol which do the following functions:
    
    check if the website url is valid to download the Favicon first before sending download request.
    use FaviconFinder to fetch the Favicon.
    run completion when with the downloaded FaviconFinder or the error that happen.
    the error could be one of the 2 following errors:
    Downloading failed for the one of the following reasons
    1. no internet
    2. emptyData
    3. failedToParseHTML
    4. failedToFindFavicon
    5. failedToDownloadFavicon
    6. emptyFavicon
    7. notValidImage
    the website url is not valid

--- DapiTask/Core/Extensions/String+Extension.swift
+++ DapiTask/Core/Extensions/String+Extension.swift
@@ -12,4 +12,9 @@ extension String {
     var localized: String {
         return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
     }
+    /// A localized value form Localizable base on current app local and passing an arguments values.
+    func localized(with arguments: [CVarArg]) -> String {
+        return String(format: self.localized, locale: nil, arguments: arguments)
+    }
+
 }
---
+++ DapiTask/Core/Favicon Downloader/FaviconDownloader.swift
@@ -0,0 +1,59 @@
+//
+//  FaviconDownloader.swift
+//  DapiTask
+//
+//  Created by MSZ on 13/07/2021.
+//  Copyright © 2021 MSZ. All rights reserved.
+//
+
+import Foundation
+import FaviconFinder
+
+class FaviconDownloader: FaviconDownloaderProtocol {
+    /// Download the Favicon from url
+    /// - important: The completion should be executed on the custom queue not the **Main**, so don't run UITask on it
+    /// - parameter urlString:  The  string value for the URL. of the website that we're trying to extract Favicon from.
+    /// - parameter completion: The completion  to call when when get the data or error.
+    ///                         This completion **should be executed on the custom queue not the main**.
+    ///  - parameter result: Result of  `Error` and `Favicon`  the model  represents  the Favicon holds imageData, and image url
+    func downloadFavicon(urlString: String, _ completion: @escaping (_ result: Result<Favicon, Error>) -> Void) {
+        guard isValidUrl(urlString),
+              let url = URL(string: urlString) else {
+            completion(.failure(NSError.FaviconDownloader.urlNotValid(urlString: urlString)))
+            return
+        }
+        FaviconFinder(url: url).downloadFavicon { result in
+            switch result {
+            case .success(let favicon):
+                if let data = favicon.image.pngData() {
+                    completion(.success(Favicon(url: url, imageData: data)))
+                } else {
+                    completion(.failure(NSError.FaviconDownloader.failedDownload(url: url)))
+                }
+            case .failure:
+                completion(.failure(NSError.FaviconDownloader.failedDownload(url: url)))
+            }
+        }
+
+    }
+
+    func isValidUrl(_ urlString: String) -> Bool {
+        let urlRegEx = "(http|https)://((\\w)*|([0-9]*)|([-|_])*)+([\\.|/]((\\w)*|([0-9]*)|([-|_])*))+"
+        return NSPredicate(format: "SELF MATCHES %@", urlRegEx).evaluate(with: urlString)
+    }
+
+}
+
+extension NSError {
+    struct FaviconDownloader {
+        static func failedDownload(url: URL) -> NSError {
+            NSError(domain: "Networking-FaviconDownloader",
+                    message: "error.faviconDownloader.failedDownload".localized(with: [url.absoluteString]))
+        }
+        static func urlNotValid(urlString: String) -> NSError {
+            NSError(domain: "Networking-FaviconDownloader",
+                    message: "error.faviconDownloader.urlNotValid".localized(with: [urlString]))
+        }
+
+    }
+}
